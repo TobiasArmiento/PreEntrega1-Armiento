d50bc6ffc7006f14be764ebf21547dffff9b9a40
Tobias Armiento <tobiasarmiento555@gmail.com>
2023-01-12 16:21:01 -0300

    rama1

---
+++ js/main.js
@@ -0,0 +1,81 @@
+// comentario simple
+
+/* 
+comentario mas de una linea 
+de la clase de javascript 
+*/
+
+// variables
+
+let edad = 5;
+edad = 15;
+
+const nombre = "rodrigo";
+
+//operaciones con numeros
+
+const numero1 = 20;
+const numero2 = 10;
+
+const suma = numero1 + numero2;
+
+console.log ("El resultado de la suma es " + suma)
+
+// multiplicacion
+
+const multiplicacion = numero1 * numero2;
+
+console.log (multiplicacion)
+
+// Division
+
+const division = numero1 / numero2;
+
+console.log (division)
+
+
+// concatenar cadenas
+
+const cadena1 = "Hola";
+const cadena2 = "Soy tobias";
+
+const concatenarString = cadena1 + cadena2; /* Esto va a quedar HolaSoy Tobias*/
+
+console.log (concatenarString)
+
+// concatenar cadenas con espacio
+
+/* const cadena3 = "Hello";
+const cadena4 = "I am Tobias";
+const espacio1 = " ";
+
+const concatenarString2 = cadena3 + espacio1 + cadena4;
+
+console.log (concatenarString2) /* Muestra el resultado de concatenarstring2 dentro de la consola del navegador web*/
+
+
+// PROMPT crea un mensaje donde el usuario debe completar con informacion
+
+//const nombre1 = prompt ("Ingrese su nombre" )
+
+// console.log ("Su nombre es " + nombre1)
+
+/* alert ("Su nombre es " + nombre1)*/
+
+
+// parseInt() and parseFloat()
+
+const numero3 = 10;
+const numero4 = "10";
+
+const resultadoSuma= numero3 + parseInt(numero4);
+
+// Ejemplo claro de utilizacion del PARSEINT
+
+console.log (resultadoSuma)
+
+const numeroIngresado = parseInt(prompt("Ingrese su numero"))
+
+const resultado2 = numeroIngresado + 10;
+
+console.log ("El resultado de prompt es " + resultado2)
