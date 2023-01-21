5dbbc3a38a357202ea9341119274336e4042dc65
Tobias Armiento <tobiasarmiento555@gmail.com>
2023-01-20 22:23:46 -0300

    final ejercicios parte 1

--- js/main.js
+++ js/main.js
@@ -150,16 +150,137 @@ console.log (concatenarString2) /* Muestra el resultado de concatenarstring2 den
 //5555555555555555555555555555555
 // Escriba un programa donde se le solicite el año de nacimiento. Si es mayor a 18 se le tiene que pedir que ingrese su nombre y apellido para mostrarle un texto con el formato: “Hola nombre apellido, tu edad actual es: edad”. Ejemplo: “Hola Rodrigo Río, tu edad actual es 25”.
 
-const anioDeNacimiento = prompt("Ingrese su año de nacimiento");
-const edad = 2023 - anioDeNacimiento;
+// const anioDeNacimiento = prompt("Ingrese su año de nacimiento");
+// const edad = 2023 - anioDeNacimiento;
 
-if (anioDeNacimiento <= 2005) {
-    let nombre = prompt("Ingrese su Nombre")
-    let apellido = prompt("Ingrese su Apellido")
+// if (anioDeNacimiento <= 2005) {
+//     let nombre = prompt("Ingrese su Nombre")
+//     let apellido = prompt("Ingrese su Apellido")
 
-    alert("Hola " + nombre + apellido + " tu edad actual es " + edad)
+//     alert("Hola " + nombre + apellido + " tu edad actual es " + edad)
+// }
+
+// else if (anioDeNacimiento > 2005){
+//     alert("Usted es menor de edad")
+// }
+
+
+
+
+//6666666666666666666666666666666
+
+// Escriba un programa que permita calcular el costo total del producto con su IVA. Para esto, solicite al usuario el precio del producto, calcule su IVA (21%) y muéstrele en pantalla con un alert el precio final.
+
+// let precioDelProducto = prompt("Indique el precio de su producto")
+
+// let ivaDelProducto = precioDelProducto * 1.21;
+
+// alert ("El precio final de su producto mas iva es " + ivaDelProducto)
+
+
+
+
+//77777777777777777777777777777777
+
+// // Escriba un programa en donde se le pida al usuario ingresar el lugar a donde quiera viajar. Teniendo en cuenta estos valores:
+// - Colombia: $40
+// - México: $50
+// - Brasil: $30
+// - Uruguay: $20
+// - Chile: $20
+// - Ecuador: $30
+// - Perú: $40
+// - Estados Unidos: $50
+// Mostrarle al usuario cuál sería el costo de viajar al lugar que escribió.
+
+// const lugarDondeViajar = prompt("Ingrese el lugar donde quiera viajar")
+
+// let costoDelViaje = 0;
+
+// if (lugarDondeViajar === "Colombia" || lugarDondeViajar === "Peru") {
+//     costoDelViaje = 40;
+// }
+// else if (lugarDondeViajar === "Mexico" || lugarDondeViajar === "Estados Unidos") {
+//     costoDelViaje = 50;
+// }
+// else if (lugarDondeViajar === "Brasil" || lugarDondeViajar === "Ecuador") {
+//     costoDelViaje = 30;
+// }
+// else if (lugarDondeViajar === "Uruguay" || lugarDondeViajar === "Chile") {
+//     costoDelViaje = 20;
+// }
+
+// console.log("El costo del viaje es " + costoDelViaje);
+
+
+
+
+
+/////////////Operadores lógicos
+
+//1111111111111111111111111111111111
+
+// // Siendo X, Y y Z valores booleanes cuyos valores son: X = true, Y = false y Z = true, determine el valor de cada expresión lógica:
+// // (X && Y) || (X && Z)
+// (T && F) || (T && T)
+//    F     ||    T 
+//          T
+
+// // (X || !Y) && (!X || Z)
+// (T || notF) && (notTrue || F)
+//     T       &&      F
+//             T
+
+// // X || Y && Z        
+// (T)  || F & T
+// T    || F
+//     T
+// // !(X || Y) && Z 
+//     F & F
+//       F
+
+
+// // X || Y || X && !Z && !Y 
+//             (T && F && T)
+//     T || F || F
+//         T
+
+// // !X || !Y || Z && X && !Y
+//    F  || T  || (T && T && T)
+//          T
+
+
+
+
+
+//22222222222222222222222222222222
+
+// Escriba un programa que le pida una letra al usuario y le diga si es una vocal o no
+
+// const Letra = prompt("Escriba una letra");
+
+// if((Letra === "a") || (Letra === "e") || (Letra === "i") || (Letra === "o") || (Letra === "u")) {
+//     console.log("La letra es una vocal");
+// }
+// else {
+//     console.log("la letra no es vocal");
+// }
+
+
+
+
+//33333333333333333333333333333333
+
+// //Escriba un programa en donde se le pida un nombre y una edad al usuario. Muestre un mensaje en pantalla si:
+// - El nombre es “Gabriel” y tiene más de 24 años
+// - El nombre es “María” y tiene entre 28 y 35 años
+
+let nombre = prompt("ingrese su nombre");
+const edad = prompt ("ingrese su edad");
+
+if((nombre === "Gabriel") && (edad > 24)) {
+    alert("Bienvenido Gabriel");
 }
-
-else if (anioDeNacimiento > 2005){
-    alert("Usted es menor de edad")
+else if ((nombre === "Maria") && ((edad >= 28) && (edad <= 35))) {
+    alert("Maria usted tiene entre 28 y 35 años");
 }
