0c02db74963b137ef20ef2439064fd0441b7f9f2
Tobias Armiento <tobiasarmiento555@gmail.com>
2023-02-06 00:04:10 +0100

    actualizacion primer preentrega

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 
 
-// let productos = [{id: 1, nombre: 'Limon', linea: 'D', tamaño: '35cc', precio: 200, cantidad: 1},
-//                 {id: 2, nombre: 'Vainilla', linea: 'D', tamaño: '35cc', precio: 200, cantidad: 2},
-//                 {id: 3, nombre: 'Chocolate', linea: 'D', tamaño: '35cc', precio: 200, cantidad: 1},
-//                 {id: 4, nombre: 'Frutilla', linea: 'D', tamaño: '35cc', precio: 200, cantidad: 1}
+// let productos = [{id: 1, nombre: 'Banana', linea: 'D', tamaño: '35cc', precio: 200, cantidad: 1},
+//                 {id: 2, nombre: 'Cacao', linea: 'D', tamaño: '35cc', precio: 200, cantidad: 2},
+//                 {id: 3, nombre: 'Dulce de Leche', linea: 'D', tamaño: '35cc', precio: 200, cantidad: 1},
+//                 {id: 4, nombre: 'Fernet', linea: 'D', tamaño: '35cc', precio: 200, cantidad: 1}
 // ];
 
 
@@ -25,7 +25,7 @@
 
 
 
-// ! creas una class con su constructor y sus objetos.
+// ! creacion de una clase con su constructor y sus objetos.
 
 class EsenciasAlimenticias {
     constructor(id, nombre, linea, tamaño, precio, cantidad) {
@@ -39,21 +39,27 @@ class EsenciasAlimenticias {
         this.subtotal = 0;
     }
 
+    // ! Se grega el iva dentro de la misma class.
     sumaIva () {
         this.precio = this.precio * 1.21;
     }
 }
 
 
+// ! Se declara el array
 const esencias = []
 
+// ! Se llama al array "esencias.push" y con "new" se agrega el nuevo producto a la clase EsenciasAlimienticias. 
 esencias.push(new EsenciasAlimenticias (1, `Limon`, `D`, `110cc`, 200, 1)),
 esencias.push(new EsenciasAlimenticias (2, 'Vainilla', 'D', '110cc', 200, 10)),
 esencias.push(new EsenciasAlimenticias (3, 'Chocolate', 'D', '110cc', 200, 1)),
 esencias.push(new EsenciasAlimenticias (4, 'Mango', 'D', '110cc', 200, 6)),
 
+// ! Con un "console.log" se visualiza el resultado del agregado de nuevos productos dentro de la clase "EsenciasAlimenticias"
 console.log(esencias);
 
+
+// ! Se crea la funcion "subtotal" para calcular este mismo y el Iva luego de tener los valores de cada objeto creando una funcion. 
 function subtotal () {
     for(const producto of esencias) {
     producto.sumaIva();
@@ -62,4 +68,5 @@ function subtotal () {
     }
 }
 
+// ! Se llama a la funcion para poder visualizarla. 
 subtotal();
