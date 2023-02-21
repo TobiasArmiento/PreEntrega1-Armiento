e05e6bbfec516e5a9a58e2c0798d662032cb3e3c
Tobias Armiento <tobiasarmiento555@gmail.com>
2023-02-21 15:49:23 +0100

    3 preentrega armiento tobiasq

--- js/main.js
+++ js/main.js
@@ -84,3 +84,145 @@
 // const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 // console.log(carrito);
+
+
+
+
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+//Inicia el programa
+
+
+function renderizarProductos (productos) {
+
+   //Limpiamos la tabla
+    bodyTabla.innerHTML = "";
+
+    productos.forEach ((producto) => {
+
+        //Cremos la fila
+        const tr = document.createElement("tr");
+
+        const tdNombre = document.createElement("td");
+
+            tdNombre.innerHTML= `${producto.nombre}`;
+
+        const tdPrecio = document.createElement("td");
+
+            tdPrecio.innerHTML = `$${producto.precio}`;
+
+        const tdAcciones = document.createElement("td");
+        
+        const botonEliminarProducto = document.createElement("button");
+            botonEliminarProducto.innerText = "Eliminar";
+
+
+        //AGREGAR EVENTO AL BOTON DE ELIMINAR
+        botonEliminarProducto.addEventListener ("click", () => {
+            const indiceElementoEliminar = ListaDeProductos.findIndex((productoAEliminar) => {
+                return productoAEliminar.nombre === producto.nombre;
+            });
+
+        // BORRO EL PRODUCTO UTILIZANDO EL INDICE
+
+        ListaDeProductos.splice (indiceElementoEliminar, 1);
+
+        actualizarLS();
+
+        renderizarProductos(ListaDeProductos);
+
+        });
+
+        tdAcciones.append(botonEliminarProducto);
+
+        //Agrego el td al tr
+
+        tr.append(tdNombre);
+        tr.append(tdPrecio);
+        tr.append(tdAcciones);
+
+        //Agrego el tr al tbody
+        bodyTabla.append(tr);
+    });
+
+}
+
+//OBTENGO LOS NUEVOS PRODUCTOS AGRAGADOS POR EL USUARIO
+function obtenerProductos () {
+    let productos = [];
+    let productosLS = localStorage.getItem("productos");
+
+
+    //SI HAY ALGO DISTINTO DE NULL LO PARSEO Y LO AGREGO A PRODUCTOS
+    if(productosLS !== null) {
+        productos = JSON.parse(productosLS);
+    }
+
+    return productos;
+}
+
+
+//ACTUALIZO EL LOCALSTORAGE CON LOS NUEVOS PRODUCTOS AGREGADOS
+function actualizarLS () {
+    
+    const ListaDeProductosJSON = JSON.stringify(ListaDeProductos);
+    localStorage.setItem("productos", ListaDeProductosJSON);
+}
+
+
+//Se crean variables llamando a los id del html para luego poder modificarlos
+const formularioCargarProductos = document.getElementById("cargarProductos");
+const inputNombre = document.getElementById("nombreDelProducto");
+const inputPrecio = document.getElementById("precioDelProducto");
+const bodyTabla = document.getElementById("bodyTabla");
+const inputBuscar = document.getElementById("buscarProducto");
+const ListaDeProductos = obtenerProductos();
+
+
+console.log(ListaDeProductos);
+
+
+//Se llama al formulario para luego almacenar los datos ingresados por el usuario
+formularioCargarProductos.addEventListener ( "submit", (event) => {
+
+  //freno el flujo
+    event.preventDefault();
+
+    //obtengo el nombre y el precio
+    const nombre = inputNombre.value;
+    const precio = inputPrecio.value;
+
+   //reseteo el valor de cada input
+    inputNombre.value = "";
+    inputPrecio.value = "";
+
+//agrego el producto ingresado por el usuario al array
+    ListaDeProductos.push(new Producto (nombre, precio));
+
+// LLAMAR A LA FUNCTION ACTUALIZARLS
+    obtenerProductos();
+    actualizarLS();
+
+    renderizarProductos(ListaDeProductos);
+});
+
+inputBuscar.addEventListener("input", () => {
+
+    const palabraABuscar = inputBuscar.value;
+
+    //FILTRADO DE LOS PRODUCTOS
+    const productosFiltrados = ListaDeProductos.filter ( (producto) => {
+        return producto.nombre.toLowerCase()
+        .includes(palabraABuscar.toLowerCase());
+    });
+
+    renderizarProductos(productosFiltrados);
+
+});
+
+renderizarProductos(ListaDeProductos);
